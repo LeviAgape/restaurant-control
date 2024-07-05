09054444ff4befa0a6ff72836f11babceb78f4a2
Levi Ágape <agapelevi99@gmail.com>
2024-07-05 12:55:22 -0300

    first commit

---
+++ src/main/java/com/example/Restaurant/controller/RestaurantController.java
@@ -0,0 +1,17 @@
+package com.example.Restaurant.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/food")
+public class RestaurantController {
+
+    @GetMapping
+    public ResponseEntity getAll(){
+        return ResponseEntity.ok("deu ok");
+    }
+}
+
